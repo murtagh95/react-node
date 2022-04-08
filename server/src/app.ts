1d84c0b5c2ec9c4b79170487106f28c899e8dc0d
Murtagh95 <nec.catalano@gmail.com>
2022-04-08 15:10:45 -0300

    feat: add morgan and ts-node-dev

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 const server = express();
+
+if(process.env.ENVIRONMENT != 'production'){
+    server.use(morgan('dev'));
+}
 
 server.get("/", (req, res) => {
     res.send("hola mundo")
